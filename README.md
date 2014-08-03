45c5563270849a4747ab8ccaadee3227442f484d
John Shovic <jshovic@wardner.com>
2014-08-03 19:32:37 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+Raspberry Pi Python Library for DS3231 RTC
+
+SwitchDoc Labs, LLC  August 3, 2014
+
+Clone respository and run testDS3231.py to test
+
+More Information on www.switchdoc.com
+
+
---
+++ testSDL_DS3231.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#
+# Test SDL_DS3231
+# John C. Shovic, SwitchDoc Labs
+# 08/03/2014
+#
+#
+
+# imports
+
+import sys
+import time
+import datetime
+
+import SDL_DS3231
+
+# Main Program
+
+print ""
+print "Test SDL_DS3231 Version 1.0 - SwitchDoc Labs"
+print ""
+print ""
+print "Program Started at:"+ time.strftime("%Y-%m-%d %H:%M:%S")
+
+filename = time.strftime("%Y-%m-%d%H:%M:%SRTCTest") + ".txt"
+starttime = datetime.datetime.utcnow()
+
+ds1307 = SDL_DS3231.SDL_DS3231(1, 0x68)
+ds1307.write_now()
+
+# Main Loop - sleeps 10 minutes, then reads and prints values of all clocks
+
+
+while True:
+
+	currenttime = datetime.datetime.utcnow()
+
+	deltatime = currenttime - starttime
+ 
+	print ""
+	print "Raspberry Pi=\t" + time.strftime("%Y-%m-%d %H:%M:%S")
+	
+	print "DS3231=\t\t%s" % ds1307.read_datetime()
+
+	time.sleep(10.0)
