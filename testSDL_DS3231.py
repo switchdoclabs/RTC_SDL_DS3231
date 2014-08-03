5cada89cb4247cd5b8e7b14c4fd1f7cc54f631d7
John Shovic <jshovic@wardner.com>
2014-08-03 19:58:23 +0000

    fixed typo in test

--- testSDL_DS3231.py
+++ testSDL_DS3231.py
@@ -25,8 +25,8 @@ print "Program Started at:"+ time.strftime("%Y-%m-%d %H:%M:%S")
 filename = time.strftime("%Y-%m-%d%H:%M:%SRTCTest") + ".txt"
 starttime = datetime.datetime.utcnow()
 
-ds1307 = SDL_DS3231.SDL_DS3231(1, 0x68)
-ds1307.write_now()
+ds3231 = SDL_DS3231.SDL_DS3231(1, 0x68)
+ds3231.write_now()
 
 # Main Loop - sleeps 10 minutes, then reads and prints values of all clocks
 
@@ -40,6 +40,6 @@ while True:
 	print ""
 	print "Raspberry Pi=\t" + time.strftime("%Y-%m-%d %H:%M:%S")
 	
-	print "DS3231=\t\t%s" % ds1307.read_datetime()
+	print "DS3231=\t\t%s" % ds3231.read_datetime()
 
 	time.sleep(10.0)
